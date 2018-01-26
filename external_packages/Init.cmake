dd192dd484b22bb88a025eed39feda3dd18a30c8
Richard Hodges <richard@trustportal.org>
2018-01-26 08:49:17 +0100

    1. update .gitignore to ignore all build directories 2. remove cmake-build-native build artifacts 3. remove Sugar submodule

--- external_packages/Init.cmake
+++ external_packages/Init.cmake
@@ -1,18 +1,4 @@
 if ("${CMAKE_SOURCE_DIR}/external_packages" STREQUAL "${CMAKE_CURRENT_LIST_DIR}")
-#[[
-    if (NOT EXISTS "${CMAKE_CURRENT_LIST_DIR}/Sugar/cmake")
-        message(STATUS "Sugar dependency not present - fetching")
-        execute_process(COMMAND git submodule update --init Sugar
-                WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/external_packages"
-                RESULT_VARIABLE result
-                OUTPUT_VARIABLE output
-                ERROR_VARIABLE error)
-        if (NOT result EQUAL 0)
-            message(FATAL_ERROR "error:\n${error}")
-        endif ()
-    endif ()
-    include("${CMAKE_SOURCE_DIR}/external_packages/Sugar/cmake/Sugar")
-]]
 
     if (DEFINED CMAKE_TOOLCHAIN_FILE)
         message(STATUS "Configuring for toolchain [${CMAKE_TOOLCHAINFILE}]")
